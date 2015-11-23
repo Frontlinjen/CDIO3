09675732d743ea271406f30b0934d5ae9b63cb16
SenadBegovic <senadbegovic2500@hotmail.com>
2015-11-23 13:51:36 +0100

    Udvidelse af konstruktøren i Territory klassn

--- src/game/Territory.java
+++ src/game/Territory.java
@@ -4,8 +4,8 @@ public class Territory extends Ownable{
 
 	private int rent;
 	
-	public Territory(int i, Types type, int price) {
-		super(i, type, price);
+	public Territory(int i, Types type, int pos, int price) {
+		super(i, type, pos, price);
 		this.rent = rent;
 		// TODO Auto-generated constructor stub
 	}

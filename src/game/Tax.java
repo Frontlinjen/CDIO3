d5bf8420b0affd882fde2ad22b08811a37cbfbbc
SenadBegovic <senadbegovic2500@hotmail.com>
2015-11-23 13:50:31 +0100

    Udvidelse af konstruktøren i Tax klassen

--- src/game/Tax.java
+++ src/game/Tax.java
@@ -5,8 +5,8 @@ public class Tax extends Field{
 	private int taxAmount;
 	private int taxRate = -1;
 	
-	public Tax(int i, Types type, int price) {
-		super(i, type);
+	public Tax(int i, Types type, int pos, int price) {
+		super(i, type, pos);
 		this.taxAmount = taxAmount;
 		// TODO Auto-generated constructor stub
 	}

d2c8b9050e10f204c917f1baf92f1102b2789d04
Nicki <Nicki@10.16.106.192>
2015-11-24 15:35:23 +0100

    Added pushToGUI method

--- src/slots/Refuge.java
+++ src/slots/Refuge.java
@@ -19,4 +19,12 @@ public class Refuge extends Field{
 	public void landOnField(Player player) {
 		player.getAccount().addGold(bonus);		
 	}
+	@Override
+	public desktop_fields.Field pushToGUI(int position){
+		desktop_fields.Field refuge = new desktop_fields.Refuge.Builder().build();
+		refuge.setDescription(this.getDescription());
+		refuge.setTitle(this.getName());
+		refuge.setSubText(this.bonus+"");
+	}
+	
 }

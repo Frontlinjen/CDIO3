bfb961d8137a3dbb45a4c19f4172cf1d71bc2b9c
Hjorthenify <Unknown>
2015-11-24 14:15:27 +0100

    Fixed import issues

--- src/game/FieldLoader.java
+++ src/game/FieldLoader.java
@@ -1,22 +1,19 @@
 package game;
+import slots.*;
 
 import java.io.File;
-import java.io.IOException;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
-import game.Field.Types;
+import slots.Field.Types;
 
 public class FieldLoader {
 	/**

4d50a6efcf92350ed814e3e378445b61a36e0ab1
Hjorthenify <Unknown>
2015-11-24 14:01:52 +0100

    Made shuffle bag generic
    This will allow us to use it with any reference type.

--- src/game/ShuffleBag.java
+++ src/game/ShuffleBag.java
@@ -2,14 +2,18 @@ package game;
 
 import java.util.Random;
 
-public class ShuffleBag {
-	int[] values;
+public class ShuffleBag<T> {
+	T[] values;
 	int currentPos;
 	Random rng = new Random(System.currentTimeMillis());
-	public ShuffleBag(int[] values)
+	public ShuffleBag(T[] values)
 	{
 		this.values = values;
 		Reset();
+	}
+	public int elementsLeft()
+	{
+		return currentPos;
 	}
 	public void Reset()
 	{
@@ -18,26 +22,26 @@ public class ShuffleBag {
 	
 	private void swapToEnd(int index)
 	{
-		int tmpValue = values[index];
+		T tmpValue = values[index];
 		values[index] = values[currentPos];
 		values[currentPos] = tmpValue;
 		//Removed the used index out of scope. 
 		--currentPos;
 	}
-	public int getNext() throws Exception
+	public T getNext() throws Exception
 	{
 		if(currentPos==-1)
 		{
-			throw new Exception("Shuffle bag has run out of free variables. If this is intended call reset before getting the next variable.");
+			throw new Exception("Shuffle bag has run out of free elements. If this is intended call reset before getting the next variable.");
 		}
 		//+1 due to the last index being exclusive
 		int index = rng.nextInt(currentPos+1);
-		int value = values[index];
+		T value = values[index];
 		swapToEnd(index);
 		return value;		
 	}
 	public static void main(String[] args) {
-		ShuffleBag bag = new ShuffleBag(new int[]{1, 2, 3, 4, 5, 6, 7, 8 , 9, 9 ,9});
+		ShuffleBag<Integer> bag = new ShuffleBag<Integer>(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8 , 9, 9 ,9});
 		try{
 			while(true)
 			{

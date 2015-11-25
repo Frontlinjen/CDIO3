50e9186fb91e66380155229a3bf77f4f77d0b988
Senad Begovic <Senadbegovic2500@hotmail.com>
2015-11-25 19:20:39 +0100

    Fixed LaborCamp class so its rent is correct

--- src/slots/LaborCamp.java
+++ src/slots/LaborCamp.java
@@ -30,6 +30,7 @@ public class LaborCamp extends Ownable{
 	@Override 
 	public void landOnField(Player player) {
 		if(checkOwner(player)){
+			baseRent = player.getDice().rollDice().getSum() * 100 * player.getProperty().getLaborCampOwned();
 			GUI.showMessage(Translator.getString("PAYTHEOWNER", baseRent));
 			player.getAccount().transferTo(getOwner().getAccount(), baseRent);
 		}else{

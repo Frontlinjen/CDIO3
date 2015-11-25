53733b060b49a5862b612328c317d8ba0c5258ae
MikkelLeth <MikkelLeth@mikkels-mbp-2.home>
2015-11-25 14:03:48 +0100

    Changed the Property class
    To keep track of the properties a player owns.

--- src/game/Property.java
+++ src/game/Property.java
@@ -1,18 +1,33 @@
 package game;
 
+import slots.Ownable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Property {
 
 	private int fleetOwned=0;
 	private int laborCampOwned=0;
+	private List<slots.Ownable> properties = new ArrayList<slots.Ownable>();
 	
-	public int getFleetOwned()
+	 
+	public Iterator<Ownable> getPropertiesOwned()
 	{
-		return fleetOwned;
+		return properties.iterator();
+	}
+	public void addProperty(Ownable p)
+	{
+		properties.add(p);
 	}
 	
 	public int getLaborCampOwned()
 	{
 		return laborCampOwned;
+	}
+	public int getFleetOwned()
+	{
+		return fleetOwned;
 	}
 	
 	public void expandFleet() {

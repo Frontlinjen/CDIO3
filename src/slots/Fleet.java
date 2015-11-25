4147d1b426b8e942372bd9884a08a977dfec6569
Nicki <Nicki@10.16.75.89>
2015-11-25 13:40:51 +0100

    Changed path

--- src/slots/Fleet.java
+++ src/slots/Fleet.java
@@ -10,7 +10,7 @@ import game.Translator;
 import slots.Field.Types;
 
 public class Fleet extends Ownable{
-	desktop_fields.Shipping fleet;
+	desktop_fields.Street fleet;
 	private final int RENT_1 = 500;
 	private final int RENT_2 = 1000;
 	private final int RENT_3 = 2000;
@@ -55,7 +55,7 @@ public class Fleet extends Ownable{
 	@Override
 	public desktop_fields.Field pushToGUI(int position) {
 		this.position = position;
-		fleet = new desktop_fields.Shipping.Builder().build();
+		fleet = new desktop_fields.Street.Builder().build();
 		fleet.setTitle(getName());
 		fleet.setDescription(getDescription());
 		fleet.setSubText("" + price);

e12275cc60d8992d47004b9dcc1046818e843876
MikkelLeth <MikkelLeth@mikkels-mbp-2.home>
2015-11-25 14:32:48 +0100

    Fixed issue where GameBoard was using the unshuffled field layout

--- src/game/GameBoard.java
+++ src/game/GameBoard.java
@@ -11,13 +11,14 @@ public class GameBoard {
 		//desktop_fields.Brewery b = new desktop_fields.Brewery.Builder().setRent("2000").build();
 		
 		System.out.println("Loading board...");
-		fields = FieldLoader.parseFields("Fields.xml");
+		Field[] tmpFields = FieldLoader.parseFields("Fields.xml");
 		for(Field f : fields)
 		{
 			System.out.println(f.getName());
 		}
-		ShuffleBag<Field> shuffle = new ShuffleBag<Field>(fields);
-		desktop_fields.Field[] guiFields = new desktop_fields.Field[fields.length];
+		ShuffleBag<Field> shuffle = new ShuffleBag<Field>(tmpFields);
+		desktop_fields.Field[] guiFields = new desktop_fields.Field[tmpFields.length];
+		fields = new Field[tmpFields.length];
 		
 		int pos = 1;
 		while(shuffle.elementsLeft()>0)
@@ -27,6 +28,7 @@ public class GameBoard {
 				f = shuffle.getNext();
 				desktop_fields.Field guiField = f.pushToGUI(pos);
 				guiFields[pos-1] = guiField;
+				fields[pos-1] = f;
 				++pos;
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
